9f1773fce933934f115edece5378e46d1bd6e9f8
Dokgo <deathlife.2k@gmail.com>
2021-12-12 22:10:41 +0700

    Sua CHuan hoa

--- CodeKPDL/Program.cs
+++ CodeKPDL/Program.cs
@@ -162,27 +162,27 @@ namespace CodeKPDL
             foreach (var item in x)
                 vx.Add(((item - minx) / (maxx - minx)) * (max - min) + min);
             Console.WriteLine($"\nX Chuan hoa min - max:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vx[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{x[i]}]={Math.Round(vx[i], 2)} \n"); }
 
             float miny = y.Min();
             float maxy = y.Max();
             foreach (var item in y)
                 vy.Add(((item - miny) / (maxy - miny)) * (max - min) + min);
             Console.WriteLine($"\nY Chuan hoa min - max:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vy[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{y[i]}]={Math.Round(vy[i], 2)} \n"); }
 
             vx.Clear();
-            foreach (var item in y)
+            foreach (var item in x)
                 vx.Add((item - meanX) / sdX);
             Console.WriteLine($"\nX Chuan hoa z-sc:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vx[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{x[i]}]={Math.Round(vx[i], 2)} \n"); }
 
 
             vy.Clear();
             foreach (var item in y)
                 vy.Add((item - meanY) / sdY);
             Console.WriteLine($"\nY Chuan hoa z-sc:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vy[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{y[i]}]={Math.Round(vy[i], 2)} \n"); }
 
 
             vx.Clear();
@@ -190,7 +190,7 @@ namespace CodeKPDL
             foreach (var item in y)
                 vx.Add((item - meanX) / sdXa);
             Console.WriteLine($"\nX Chuan hoa z-sc Lech chuan tuyet doi:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vx[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{x[i]}]={Math.Round(vx[i], 2)} \n"); }
 
 
             vy.Clear();
@@ -198,7 +198,7 @@ namespace CodeKPDL
             foreach (var item in y)
                 vy.Add((item - meanY) / sdYa);
             Console.WriteLine($"\nY Chuan hoa z-sc Lech chuan tuyet doi:");
-            for (int i = 0; i < n; i++) { Console.Write($"v'[{i + 1}]={Math.Round(vy[i], 2)} \n"); }
+            for (int i = 0; i < n; i++) { Console.Write($"v'[{y[i]}]={Math.Round(vy[i], 2)} \n"); }
         }
 
         public void CalHeSoTuongQuan()
